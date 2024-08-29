22c38f870fd099f7993078e748f945d7853c4ad7
yousung.ahn <nug22kr@gmail.com>
2024-08-29 23:30:08 +0900

    first init

---
+++ script.js
@@ -0,0 +1,9 @@
+// 페이지 이동 시 부드러운 스크롤 구현
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
